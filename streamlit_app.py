368c305d2ab035b3349c3edfe24a6e1e9ee7ba5b
rohindadina <rohind.2008@gmail.com>
2023-06-21 18:15:50 +0530

    Update streamlit_app.py
    added additional code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parents New healthy diner')
+streamlit.header('Breakfast menu)
+streamlit.text('Omega 3 and BB oat')
+streamlit.text('Kale Spinach and Rocket smoothie')
+streamlit.text('Hard boiled free range')
+                 
+                 
