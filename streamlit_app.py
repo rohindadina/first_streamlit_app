9f8e9266ff75e5118ef2a6df18680a72a3e1eb9e
rohindadina <rohind.2008@gmail.com>
2023-06-21 18:17:17 +0530

    Update streamlit_app.py
    corrected typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New healthy diner')
-streamlit.header('Breakfast menu)
+streamlit.header('Breakfast menu')
 streamlit.text('Omega 3 and BB oat')
 streamlit.text('Kale Spinach and Rocket smoothie')
 streamlit.text('Hard boiled free range')
